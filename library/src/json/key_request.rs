5a1d8e2d4a5c7f8126c5cd22616746dd1fc36171
Nick Aquina <n.aquina@tue.nl>
2024-11-28 11:30:37 +0100

    Omit JSON item instead of using null in optional items

--- library/src/json/key_request.rs
+++ library/src/json/key_request.rs
@@ -4,8 +4,10 @@ use serde_json::{Map, Value};
 #[derive(Debug, PartialEq, Eq, Clone, Serialize)]
 pub struct KeyRequest<'a> {
     pub number: u32,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub size: Option<u32>,
     #[serde(rename(deserialize = "additional_slave_SAE_IDs"))]
     pub additional_target_sae_ids: &'a [&'a str],
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub extension_mandatory: Option<&'a [Map<String, Value>]>,
 }
